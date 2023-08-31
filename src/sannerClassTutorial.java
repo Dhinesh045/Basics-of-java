cfa58262a61ed517d58758755bdeb57a237b9ecf
dhineshkumarm045@gmail.com <dhineshkumarm045@gmail.com>
2023-08-31 17:20:48 +0530

    javaLearn

---
+++ src/sannerClassTutorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sannerClassTutorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String s = sc.nextLine();
+        System.out.println("Enter the number: ");
+        int i=sc.nextInt();
+
+        System.out.println("number is "+ i);
+        System.out.println("Your name is: "+s);
+
+    }
+}
