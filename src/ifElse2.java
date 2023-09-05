3de8ec89acc808785596623f2809a954d7ddf8a5
dhineshkumarm045@gmail.com <dhineshkumarm045@gmail.com>
2023-09-05 12:37:41 +0530

    ifElseExample

--- src/ifElse2.java
+++ src/ifElse2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ifElse2 {
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class ifElse2 {
+    public static void main(String[] args) {
+        System.out.println(" enter a four digit number ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int fourthDigit = i % 10;
+        int thirdDigit = (i / 10) % 10;
+        int secondDigit = (i / 100) % 10;
+        int firstDigit = (i / 1000);
+        if (i < 999 || i>9999) {
+            System.out.println(" please enter a four digit number");
+        } else {
+            if (fourthDigit + thirdDigit == secondDigit + firstDigit) {
+                System.out.println("lucky");
+            } else {
+                System.out.println("not lucky");
+            }
+        }
+    }
 }
